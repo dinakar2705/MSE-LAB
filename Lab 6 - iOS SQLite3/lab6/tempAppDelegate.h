6fa2318f5b279a1923ae843530e2263c5e6c17d9
dinakar2705 <dinakar2705@gmail.com>
2013-09-28 22:11:51 +0530

    ios prg

---
+++ Lab 6 - iOS SQLite3/lab6/tempAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  tempAppDelegate.h
+//  lab6
+//
+//  Created by MSE on 27/09/13.
+//  Copyright (c) 2013 MSE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class tempViewController;
+
+@interface tempAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) tempViewController *viewController;
+
+@end
